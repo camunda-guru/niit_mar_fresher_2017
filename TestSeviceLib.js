4bae4a5db28e74b8ecd32c8911b05029bd07538e
eswaribala <Parameswaribala@gmail.com>
2017-04-06 21:05:31 +0530

    Add files via upload

--- TestSeviceLib.js
+++ TestSeviceLib.js
@@ -24,32 +24,35 @@ describe('Service Library Test Suite',function()
         var httpResponse = [{ "stateName": "TN" }, { "stuffId": "AP" }];
         httpBackend.expectGET("https://www.whizapi.com/api/v2/util/ui/in/indian-states-list?project-app-key=vlubq5he9ngkiikqroseoxsf").respond(200, httpResponse);
 
-        service.stateServiceObj().then(function(response) {
-
-            expect(response[0].data.stateName).toBe("AP");
-
-        });
-
-        /*
-        service.stateServiceObj().then(function(response) {
-            expect(response.length).toBeGreaterThan(0);
-            $httpBackend.flush();
-
+        var test = service.stateServiceObj();
+        var result;
+        test.then(function (response) {
+            result = response.data;
 
         });
-        */
+
+        httpBackend.flush();
+
+        expect(result[0].stateName).toEqual("TN");
+
     })
     it('should contain a response having length >0 in  StateService',function()
     {
 
+        var httpResponse = [{ "stateName": "TN" }, { "stuffId": "AP" }];
+        httpBackend.expectGET("https://www.whizapi.com/api/v2/util/ui/in/indian-states-list?project-app-key=vlubq5he9ngkiikqroseoxsf").respond(200, httpResponse);
+        var test = service.stateServiceObj();
+        var result;
+        test.then(function (response) {
+            result = response.data;
+
+        });
+
+        httpBackend.flush();
+
+        expect(result).toEqual(httpResponse);
 
 
-         service.stateServiceObj().then(function(response) {
-         expect(response.length).toBeGreaterThan(0);
-         $httpBackend.flush();
-
-
-         });
 
     })
 
